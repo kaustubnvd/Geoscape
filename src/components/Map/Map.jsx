2109135362927f88382cc220fa29b5e0202298f3
Kaustub Navalady <kaustub.nvd@gmail.com>
2020-08-12 19:08:38 -0500

    ✨ Add hover interface

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -7,23 +7,64 @@ import history from '../../utils/history';
 import { getLocationName } from '../../utils/location';
 import './Map.scss';
 
-const WorldMap = () => {
-  
-  function handleMouseOver(event) {
-    console.log(event.target.attributes.name.value);
-  }
+class WorldMap extends React.Component {
+  state = {
+    hovering: false,
+    country: null,
+    x: 0,
+    y: 0,
+  };
 
-  function handleLocationClick(event) {
+  handleMouseMove = (event) => {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY,
+    });
+  };
+
+  handleMouseOver = (event) => {
+    this.setState({
+      hovering: true,
+      country: getLocationName(event),
+    });
+  };
+
+  handleLocationMouseOut = (event) => {
+    this.setState({
+      hovering: false,
+      country: null,
+    });
+  };
+
+  handleLocationClick = (event) => {
     history.push(`/${getLocationName(event)}`);
-  }
+  };
 
-  return (
-    <SVGMap
-      map={World}
-      onLocationMouseOver={(event) => handleMouseOver(event)}
-      onLocationClick={(event) => handleLocationClick(event)}
-    ></SVGMap>
-  );
-};
+  render() {
+    const { hovering, country, x, y } = this.state;
+    const tooltipPosition = {
+      top: `${y - 20}px`,
+      left: `${x + 20}px`,
+    };
+
+    return (
+      <div onMouseMove={(event) => this.handleMouseMove(event)}>
+        <SVGMap
+          map={World}
+          onLocationMouseOver={(event) => this.handleMouseOver(event)}
+          onLocationMouseOut={(event) => this.handleLocationMouseOut(event)}
+          onLocationClick={(event) => this.handleLocationClick(event)}
+        ></SVGMap>
+        {hovering && (
+          <div className="tooltip-container">
+            <span id="tooltip" className="tooltip" style={tooltipPosition}>
+              {country}
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 export default WorldMap;
